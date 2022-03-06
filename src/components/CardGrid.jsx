ece27c50cac7c8cb8c9d345ea2eec91e1b8e4c3c
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-03-06 16:21:27 +0100

    made a working two-way-binding in card component

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,8 +1,19 @@
 import styles from './CardGrid.module.css';
 import SingleCard from './SingleCard'
 export default function CardGrid(props){
+    const hiraganaArray = [
+        
+    ]
+
+    const hiraganaCards = hiraganaArray.map((hiragana)=>{
+        return <SingleCard kana={hiragana} key={hiragana.toString()}/>
+    })
     return(
-        <SingleCard/>
+        <div className="Card_Grid">
+            <SingleCard kana={'あ'} romaji={'a'}/>
+             {hiraganaCards}
+        </div>
+       
     )
     
 }
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,11 +1,25 @@
 import styles from './SingleCard.module.css'
 import Input from './UI/Input'
-import {Fragment} from 'react'
+import {Fragment,useState} from 'react'
+
 export default function SingleCard(props){
+
+    const [answered,setAnswered] = useState(false)
+    const [enteredRomaji, setEnteredRomaji] = useState('')
+    const romajiChangeHandler = (event) => {
+        setEnteredRomaji(event.target.value)
+        if(event.target.value === props.romaji){
+            setAnswered(true)
+        }
+        else{
+            setAnswered(false)
+        }
+        console.log(enteredRomaji)
+    }
     return(
-        <div className={styles.card}>
-            <div>あ</div> 
-            <Input/>
+        <div className={`${styles.card} ${answered && styles['card--answered']}`}>
+            <div>{props.kana} </div> 
+            <input type="text" value={enteredRomaji} onChange={romajiChangeHandler} />
         </div>
     )
 }

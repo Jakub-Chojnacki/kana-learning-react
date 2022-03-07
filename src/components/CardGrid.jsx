0204776128b2ab717f4a7596b851fd4dae52a3e5
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-03-07 20:29:18 +0100

    added dynamic styling to the card

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,17 +1,30 @@
 import styles from './CardGrid.module.css';
 import SingleCard from './SingleCard'
+import {useState} from 'react'
 export default function CardGrid(props){
-    const hiraganaArray = [
-        
-    ]
+    const [data,setData] =useState({})
 
-    const hiraganaCards = hiraganaArray.map((hiragana)=>{
-        return <SingleCard kana={hiragana} key={hiragana.toString()}/>
-    })
+   const getData = () => {
+       fetch('/public/kana.json')
+       .then(res=> {
+           return res.json();
+       })
+       .then(kana => {
+           console.log(kana.k.a.Seion.Hiragana)
+           setData(kana)
+       })
+   }
+
+ 
+    // const hiraganaCards = kana.map((hiragana)=>{
+    //     return <SingleCard kana={hiragana.k.a.Seion.Hiragana} key={hiragana.toString()}/>
+    // })
     return(
         <div className="Card_Grid">
             <SingleCard kana={'あ'} romaji={'a'}/>
-             {hiraganaCards}
+            <SingleCard kana={'す'} romaji={'su'}/>
+            {/* {hiraganaCards} */}
+             <button onClick={getData}>getkana</button>
         </div>
        
     )
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,25 +1,58 @@
 import styles from './SingleCard.module.css'
 import Input from './UI/Input'
-import {Fragment,useState} from 'react'
+import {Fragment,useState,useReducer} from 'react'
 
 export default function SingleCard(props){
 
-    const [answered,setAnswered] = useState(false)
+    const ACTIONS = {
+        CORRECT: 'correct',
+        WRONG: 'wrong',
+        RESET: 'reset'
+    }
+    function reducer(state,action){
+        switch(action.type){
+            case ACTIONS.CORRECT:
+                return {answered:true, correct: true }
+            case ACTIONS.WRONG:
+                return { answered:true, correct: false}
+            case ACTIONS.RESET:
+                return {answered:false}    
+            default:
+                return state    
+        }
+    }
+    function blurCardHandler(){
+        setFocused(false)
+    }
+    function focusCardHandler(){
+        setFocused(true)
+    }
+
+    const [answer,dispatch] = useReducer(reducer, {answered: false})
     const [enteredRomaji, setEnteredRomaji] = useState('')
+    const [focused,setFocused] = useState(false)
     const romajiChangeHandler = (event) => {
+        
         setEnteredRomaji(event.target.value)
-        if(event.target.value === props.romaji){
-            setAnswered(true)
+      
+    }
+    const romajiValidationHandler = (event) => {
+        
+        if(event.target.value === props.romaji && event.target.value.trim() != ''){
+            dispatch({type:ACTIONS.CORRECT})
         }
-        else{
-            setAnswered(false)
+        else if(event.target.value != props.romaji && event.target.value.trim() !=''){
+            dispatch({type:ACTIONS.WRONG})
         }
-        console.log(enteredRomaji)
+        else {
+            dispatch({type:ACTIONS.RESET})
+        }
+        
     }
     return(
-        <div className={`${styles.card} ${answered && styles['card--answered']}`}>
+        <div onFocus={focusCardHandler} onBlur={blurCardHandler} className={`${styles.card} ${answer.correct && answer.answered && styles['card--answered']} ${!answer.correct && answer.answered && styles['card--wrong']} ${focused && styles['card--focused']}`}>
             <div>{props.kana} </div> 
-            <input type="text" value={enteredRomaji} onChange={romajiChangeHandler} />
+            <input className={styles.input} type="text" value={enteredRomaji} onChange={romajiChangeHandler} onBlur={romajiValidationHandler} disabled={answer.correct} />
         </div>
     )
 }
